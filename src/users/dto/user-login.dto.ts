9f4424085a1546bd30bc2227fcc9fab4272b9301
Grin Pavel <pashkagrin.tapston@gmail.com>
2023-03-13 03:17:21 +0300

    user login

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsString } from 'class-validator';
 export class UserLoginDto {
+	@IsEmail({}, { message: 'Неверный email' })
 	email: string;
+
+	@IsString({ message: 'Не указан пароль' })
 	password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,13 +25,26 @@ export class UserController extends BaseController implements IUserController {
 				func: this.register,
 				middlewares: [new ValidateMiddleware(UserRegisterDto)],
 			},
-			{ path: '/login', method: 'post', func: this.login },
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
 		]);
 	}
 
-	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction) {
-		console.log(req.body);
-		this.ok(res, 'login');
+	async login(
+		req: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.validateUser(req.body);
+		if (!result) {
+			return next(new HTTPError(401, 'Ошибка авторизации'));
+		} else {
+			return this.ok(res, {});
+		}
 	}
 
 	async register(
